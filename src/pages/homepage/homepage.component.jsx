ee5aaf4651c91534e282158ef87eeefe9fb17789
Dragan Belegic <www.belegic@gmail.com>
2022-02-04 18:51:53 +0100

    broken homepage into smaller components

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,15 @@
+
+
+import Directory from "../../components/directory/directory.component"
+import "./homepage.styles.scss"
+
+
+const Home = () => {
+    return(
+        <div className="homepage">
+            <Directory />
+        </div>
+   )
+}
+
+export default Home
