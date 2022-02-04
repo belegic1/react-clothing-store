0ba8df376bd08887f64ec02491c773ecfb2a2504
Dragan Belegic <www.belegic@gmail.com>
2022-02-04 22:32:13 +0100

    Adding shop page to my app

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import React from 'react';
 import {  Routes, Route} from "react-router-dom"
 import './App.css';
 import Home from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
     <div>
         <Routes>
             <Route exact path="/" element={<Home />} />
+            <Route exact path="shop" element={<ShopPage />} />
         </Routes>
     </div>
   );
