f115b9c12a71efff01adebabdb357076fecf3a48
Dragan Belegic <www.belegic@gmail.com>
2022-02-05 20:13:26 +0100

    gruping items in cart reducer

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import "./custom-buton.styles.scss"
 
-const CustomButton = ({children,isGoogleSignIn ,otherProps}) => {
-    return <button {...otherProps} className={`${isGoogleSignIn ? "google-sign-in": ""} custom-button`}>{ children}</button>
+const CustomButton = ({children,isGoogleSignIn, inverted ,otherProps}) => {
+    return <button {...otherProps} className={`${inverted ? "inverted":""}${isGoogleSignIn ? "google-sign-in": ""} custom-button`}>{ children}</button>
 };
 
 export default CustomButton;
