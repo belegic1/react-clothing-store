2292ea1499e6ea14ed07aef0a1e5dda6943332a9
Dragan Belegic <www.belegic@gmail.com>
2022-02-05 14:22:31 +0100

    Adding sign in with email and password

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import "./sign-in.styles.scss"
 import FormInput from "../form-input/form-input.component"
 import CustomButton from '../custom-button/custom-button.component';
-import { signInWithGoogle } from '../../firebase/firebase.utils';
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
 
 export default class SignIn extends Component {
     constructor() {
@@ -13,9 +13,16 @@ export default class SignIn extends Component {
             password: ""
         }
     }
-    handleSubmit = e => {
+    handleSubmit = async e => {
         e.preventDefault()
-        this.setState({email: "", password: ""})
+        const { email, password } = this.state
+        
+        try {
+            await auth.signInWithEmailAndPassword(email, password)
+            this.setState({emial:"",password: ""})
+        } catch (error) {
+            console.error(error)
+        }
 
    
     }
